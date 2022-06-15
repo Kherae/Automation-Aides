147123e60c69a4c75c7d6cd25b809d500b133dcf
Kherae <cwaldridge@live.com>
2022-06-15 13:10:56 -0700

    indentation

--- objects/kheAA/kheAA_terminal/kheAA_terminal.lua
+++ objects/kheAA/kheAA_terminal/kheAA_terminal.lua
@@ -2,15 +2,15 @@ require "/scripts/kheAA/transferUtil.lua"
 
 function init()
 	transferUtil.init()
-	message.setHandler("transferUtil.sendConfig",transferUtil.sendConfig);
-	message.setHandler("transferItem", transferItem);
-	object.setInteractive(true);
+	message.setHandler("transferUtil.sendConfig", transferUtil.sendConfig)
+	message.setHandler("transferItem", transferItem)
+	object.setInteractive(true)
 end
 
 function update(dt)
-	deltaTime=(deltaTime or 0)+dt
+	deltaTime = (deltaTime or 0) + dt
 	if deltaTime >= 1 then
-		deltaTime=0
+		deltaTime = 0
 	else
 		return
 	end
@@ -18,24 +18,20 @@ function update(dt)
 end
 
 function transferItem(_, _, itemData)
-	--dbg(itemData)
-	--itemData={containerID, index}, itemDescriptor, itemConfig,pos
-	local source = itemData[1][1];
-	if type(source)=="string"then
-		source=tonumber(source)
-	end
-	local sourcePos=itemData[4]
-	local targetPos=itemData[5]
-	local slot = itemData[1][2];
-	local count= itemData[2].count
-	local awake,newId=transferUtil.containerAwake(source,sourcePos)
-	if not awake==true then
+	-- dbg(itemData)
+	-- itemData={containerID, index}, itemDescriptor, itemConfig,pos
+	local source = itemData[1][1]
+	if type(source) == "string" then source = tonumber(source) end
+	local sourcePos = itemData[4]
+	local targetPos = itemData[5]
+	local slot = itemData[1][2]
+	local count = itemData[2].count
+	local awake, newId = transferUtil.containerAwake(source, sourcePos)
+	if awake ~= true then
 		return
 	else
-		if newId~=nil then
-			source=newId
-		end
-		local item = world.containerTakeNumItemsAt(source, slot - 1,count);
+		if newId ~= nil then source = newId end
+		local item = world.containerTakeNumItemsAt(source, slot - 1, count)
 		if item ~= nil then
 			world.spawnItem(item, targetPos or entity.position())
 		end
@@ -43,15 +39,15 @@ function transferItem(_, _, itemData)
 end
 
 function transferUtil.sendConfig()
-	local buffer={}
-	for id,pos in pairs(transferUtil.vars.inContainers) do
-		local awake,newId=transferUtil.containerAwake(id,pos)
+	local buffer = {}
+	for id, pos in pairs(transferUtil.vars.inContainers or {}) do
+		local awake, newId = transferUtil.containerAwake(id, pos)
 		if awake then
-			id=newId or id
-			buffer[id]=pos
+			id = newId or id
+			buffer[id] = pos
 		end
 	end
-		transferUtil.vars.inContainers=buffer
+	transferUtil.vars.inContainers = buffer
 	return transferUtil.vars
 end
 
